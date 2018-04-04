26db315a011bbffec202d78a8f67b60d2abdc53a
samalbertson3 <samalbertson3@gmail.com>
2018-04-04 14:00:03 -0700

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,15 @@
+library(lubridate)
+
+dat <- read.table("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors = FALSE)
+dat$Date <- dmy(dat$Date)
+dat$Time <- hms(dat$Time)
+dat <- subset(dat,Date>=ymd("2007-02-01")&Date<=ymd("2007-02-02"))
+dat$Global_active_power <- as.numeric(dat$Global_active_power)
+dat$Global_reactive_power <- as.numeric(dat$Global_reactive_power)
+
+png("plot3.png",width=480,height=480)
+plot(dat$Date+dat$Time,dat$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering",yaxp=c(0,30,3),ylim=c(-1,38))
+lines(dat$Date+dat$Time,as.numeric(dat$Sub_metering_2),type="l",col="red")
+lines(dat$Date+dat$Time,dat$Sub_metering_3,type="l",col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),pch="-",lwd=1)
+dev.off()
