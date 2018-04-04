733638a54ba53951e31663afb2c80454220fdd6b
samalbertson3 <samalbertson3@gmail.com>
2018-04-04 13:59:33 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+library(lubridate) 
+
+dat <- read.table("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors = FALSE)
+dat$Date <- dmy(dat$Date)
+dat <- subset(dat,Date>=ymd("2007-02-01")&Date<=ymd("2007-02-02"))
+dat$Global_active_power <- as.numeric(dat$Global_active_power)
+dat$Global_reactive_power <- as.numeric(dat$Global_reactive_power)
+
+png("plot1.png",width=480,height=480)
+hist(as.numeric(dat$Global_active_power),col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
